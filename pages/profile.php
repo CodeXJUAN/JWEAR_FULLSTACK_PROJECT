e525e6a91e1d7a1b7ef3a8ce13239b7717f5161a
Juan Manuel López Arrieta <juanmanuellopezarrieta@gmail.com>
2025-02-02 11:33:49 +0100

    cambiar los estilos de profile.php

--- pages/profile.php
+++ pages/profile.php
@@ -1,119 +1,160 @@
 <?php
 include('../db.php');
 session_start();
-// Consultar los datos de las imágenes
+
 $sql_otherimg = "SELECT * FROM OTHER_IMAGES";
-$sql_infousu = "SELECT * FROM USUARIOS";
-
 $result_otherimg = $pdo->query($sql_otherimg);
-$result_infousu = $pdo->query($sql_infousu);
-
 $images_otherimg = $result_otherimg->fetchAll();
-$info_infousu = $result_infousu->fetchAll();
+
+// Verificar si el usuario está logueado
+if (!isset($_SESSION['ID_USUARIO'])) {
+    header("Location: /pages/auth/login.php");
+    exit();
+}
+
+// Obtener el ID del usuario desde la sesión
+$id_usuario = $_SESSION['ID_USUARIO'];
+
+// Consultar los datos del usuario actual
+$sql_usuario = "SELECT * FROM USUARIOS WHERE ID_USUARIO = ?";
+$stmt_usuario = $pdo->prepare($sql_usuario);
+$stmt_usuario->execute([$id_usuario]);
+$info_usuario = $stmt_usuario->fetch();
+
+// Procesar el formulario de actualización
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update_profile'])) {
+    $nuevo_email = $_POST['nuevo_email'];
+    $contrasena_actual = $_POST['contrasena_actual'];
+    $nueva_contrasena = $_POST['nueva_contrasena'];
+
+    // Validar que la contraseña actual sea correcta
+    if (password_verify($contrasena_actual, $info_usuario['CONTRASENA'])) {
+        // Actualizar el correo electrónico si se proporciona uno nuevo
+        if (!empty($nuevo_email)) {
+            $sql_update_email = "UPDATE USUARIOS SET EMAIL = ? WHERE ID_USUARIO = ?";
+            $stmt_update_email = $pdo->prepare($sql_update_email);
+            $stmt_update_email->execute([$nuevo_email, $id_usuario]);
+        }
+
+        // Actualizar la contraseña si se proporciona una nueva
+        if (!empty($nueva_contrasena)) {
+            $hashed_password = password_hash($nueva_contrasena, PASSWORD_DEFAULT);
+            $sql_update_password = "UPDATE USUARIOS SET CONTRASENA = ? WHERE ID_USUARIO = ?";
+            $stmt_update_password = $pdo->prepare($sql_update_password);
+            $stmt_update_password->execute([$hashed_password, $id_usuario]);
+        }
+
+        echo "<div class='message'><p>Perfil actualizado correctamente!</p></div>";
+    } else {
+        echo "<div class='message'><p>La contraseña actual es incorrecta.</p></div>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>JWEAR - Hoddies</title>
-        <link rel="stylesheet" href="/CSS/style.css" />
-        <link rel="stylesheet" href="/CSS/mediaqueries.css" />
-        <link rel="shortcut icon" href="<?php echo $images_otherimg[0]['IMAGEN']; ?>" type="image/x-icon">
-    </head>
-    <body>
-    <header>
-        <nav id="desktop-nav">
-            <img class="logo" src="<?php echo $images_otherimg[0]['IMAGEN']; ?>" />
-            <div>
-                <ul class="nav-links">
-                    <li><a class="anav" href="/index.php">Inicio</a></li>
-                    <li><a class="anav" href="/pages/hoddie.php">Hoddies</a></li>
-                    <li><a class="anav" href="/pages/sudadera.php">Sudaderas</a></li>
-                    <li><a class="anav" href="/pages/cami.php">Camisetas</a></li>
-                    <li><a class="anav" href="/pages/contact.php">Contacto</a></li>
-                </ul>
-            </div>
-            <div>
-                <?php if (!empty($_SESSION['ID_USUARIO']) && is_numeric($_SESSION['ID_USUARIO'])): ?>
-                    <a href="/pages/profile.php"><img class="nav_img" src="/assets/usuario_ini.png" alt="Perfil"></a>
-                    <a href="/pages/auth/logout.php"><img class="nav_img" src="/assets/ingresar.png" alt="Cerrar Sesion"></a>
-                <?php else: ?>
-                    <a href="/pages/auth/login.php"><img class="nav_img" src="/assets/usuario.png" alt="Iniciar sesión"></a>
-                <?php endif; ?>
-                <button class="nav_buttons"><img class="nav_img" src="/assets/buscar.png"></button>
-                <button class="nav_buttons"><img class="nav_img" src="/assets/bolsa-de-la-compra.png"></button>
-            </div>
-        </nav>    
-    </header>   
-    <main>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JWEAR - Perfil</title>
+    <link rel="stylesheet" href="/CSS/style.css" />
+    <link rel="stylesheet" href="/CSS/mediaqueries.css" />
+    <link rel="shortcut icon" href="<?php echo $images_otherimg[0]['IMAGEN']; ?>" type="image/x-icon">
+</head>
+<body>
+<header>
+    <nav id="desktop-nav">
+        <img class="logo" src="<?php echo $images_otherimg[0]['IMAGEN']; ?>" />
         <div>
-            <h1 class="title2">PROFILE</h1>
-        </div> 
+            <ul class="nav-links">
+                <li><a class="anav" href="/index.php">Inicio</a></li>
+                <li><a class="anav" href="/pages/hoddie.php">Hoddies</a></li>
+                <li><a class="anav" href="/pages/sudadera.php">Sudaderas</a></li>
+                <li><a class="anav" href="/pages/cami.php">Camisetas</a></li>
+                <li><a class="anav" href="/pages/contact.php">Contacto</a></li>
+            </ul>
+        </div>
         <div>
-            <div class="divusu">
+            <?php if (!empty($_SESSION['ID_USUARIO']) && is_numeric($_SESSION['ID_USUARIO'])): ?>
+                <a href="/pages/profile.php"><img class="nav_img" src="/assets/usuario_ini.png" alt="Perfil"></a>
+                <a href="/pages/auth/logout.php"><img class="nav_img" src="/assets/ingresar.png" alt="Cerrar Sesion"></a>
+            <?php else: ?>
+                <a href="/pages/auth/login.php"><img class="nav_img" src="/assets/usuario.png" alt="Iniciar sesión"></a>
+            <?php endif; ?>
+            <button class="nav_buttons"><img class="nav_img" src="/assets/buscar.png"></button>
+            <button class="nav_buttons"><img class="nav_img" src="/assets/bolsa-de-la-compra.png"></button>
+        </div>
+    </nav>    
+</header>   
+<main>
+    <div>
+        <h1 class="title2">PERFIL</h1>
+    </div> 
+    <div>
+        <div class="divusu">
+            <div class="img_info">
                 <div>
                     <img class="usuimg" src="./../assets/usuario_ini.png">
                 </div>
                 <div>
-                    <p><?php echo $info_infousu[0]['NOMBRE']; ?></p>
-                    <p><?php echo $info_infousu[0]['APELLIDO']; ?></p>
+                    <p><?php echo htmlspecialchars($info_usuario['NOMBRE']); ?></p>
+                    <p><?php echo htmlspecialchars($info_usuario['APELLIDO']); ?></p>
                 </div>
             </div>
-        </div>
-        <div>
             <div>
-                <h1 class="change_passwd">CAMBIAR EMAIL</h1>      
-            </div> 
-            <div class="divusu">
-                <div>
-                    <p>Email Actual</p>
-                    <p><?php echo $info_infousu[0]['EMAIL']; ?></p>
-                </div>
-                <div>
-                    <p>Email Nuevo</p>
-                    <input type="email">
-                </div>
-                <div>
-                    <button>CHANGE</button>
-                </div>
+                <p class="pprof">Email Actual</p>
+                <p><?php echo htmlspecialchars($info_usuario['EMAIL']); ?></p>
             </div>
         </div>
+        
+    </div>
+    <div>
         <div>
-            <div>
-                <h1 class="change_passwd">CAMBIAR CONTRASEÑA</h1>      
-            </div> 
-            <div class="divusu">
+            <h1 class="change_passwd">CAMBIAR EMAIL / CONTRASEÑA</h1>      
+        </div> 
+        <form method="POST" action="">
+            <div class="divusu2">
                 <div>
-                    <label for="oldpasswd">Contraseña Actual</label>
-                    <input type="password" name="oldpasswd" id="">
+                    <div class="form__group field">
+                        <input 
+                        <input  class="form__field" placeholder="Email Nuevo" required="">
+                        <label for="nuevo_email" class="form__label">Email Nuevo</label>
+                    </div>
                 </div>
                 <div>
-                    <label for="newpasswd">Contraseña Nueva</label>
-                    <input type="password" name="newpasswd" id="">
+                    <div class="form__group field">
+                        <input type="password" name="contrasena_actual" class="form__field" placeholder="Contraseña Actual" required="">
+                        <label for="contrasena_actual" class="form__label">Contraseña Actual</label>
+                    </div>
                 </div>
                 <div>
-                    <button>CHANGE</button>
+                    <div class="form__group field">
+                        <input type="password" name="nueva_contrasena" class="form__field" placeholder="Contraseña Nueva" required="">
+                        <label for="nueva_contrasena" class="form__label">Contraseña Nueva</label>
+                    </div>
+                </div>
+                <div>
+                    <button class="button_signup" type="submit" name="update_profile">CAMBIAR</button>
                 </div>
             </div>
-        </div>
-    </main>
-    <footer>
-        <nav>
-            <div class="nav-links-container">
-                <ul class="nav-links">
+        </form>
+    </div>
+</main>
+<footer>
+    <nav>
+        <div class="nav-links-container">
+            <ul class="nav-links">
                 <li><a class="anav" href="/index.php">Inicio</a></li>
-                    <li><a class="anav" href="/pages/hoddie.php">Hoddies</a></li>
-                    <li><a class="anav" href="/pages/sudadera.php">Sudaderas</a></li>
-                    <li><a class="anav" href="/pages/cami.php">Camisetas</a></li>
-                    <li><a class="anav" href="/pages/contact.php">Contacto</a></li>
-                </ul>
-            </div>
-        </nav>  
-        <p>Copyright &#169; 2024 Juan Manuel López. All Rights Reserved.</p>
-    </footer>
-    <script src="../JS/app.js"></script>
-    </body>
+                <li><a class="anav" href="/pages/hoddie.php">Hoddies</a></li>
+                <li><a class="anav" href="/pages/sudadera.php">Sudaderas</a></li>
+                <li><a class="anav" href="/pages/cami.php">Camisetas</a></li>
+                <li><a class="anav" href="/pages/contact.php">Contacto</a></li>
+            </ul>
+        </div>
+    </nav>  
+    <p>Copyright &#169; 2024 Juan Manuel López. All Rights Reserved.</p>
+</footer>
+<script src="../JS/app.js"></script>
+</body>
 </html>
